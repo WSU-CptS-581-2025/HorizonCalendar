834edd0499a9bf2355ccc14d19b3f11d356a5d23
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-04-13 13:20:23 -0700

    Fix spacing (MonthHeaderView)

--- Sources/Public/ItemViews/MonthHeaderView.swift
+++ Sources/Public/ItemViews/MonthHeaderView.swift
@@ -19,165 +19,150 @@ import UIKit
 
 /// A view that represents a day-of-the-week header in a calendar month. For example, Sun, Mon, Tue, etc.
 public final class MonthHeaderView: UIView {
+    // MARK: Lifecycle
 
-  // MARK: Lifecycle
+    fileprivate init(invariantViewProperties: InvariantViewProperties) {
+        self.invariantViewProperties = invariantViewProperties
 
-  fileprivate init(invariantViewProperties: InvariantViewProperties) {
-    self.invariantViewProperties = invariantViewProperties
+        label = UILabel()
+        label.font = invariantViewProperties.font
+        label.textAlignment = invariantViewProperties.textAlignment
+        label.textColor = invariantViewProperties.textColor
 
-    label = UILabel()
-    label.font = invariantViewProperties.font
-    label.textAlignment = invariantViewProperties.textAlignment
-    label.textColor = invariantViewProperties.textColor
+        super.init(frame: .zero)
 
-    super.init(frame: .zero)
+        isUserInteractionEnabled = false
 
-    isUserInteractionEnabled = false
+        backgroundColor = invariantViewProperties.backgroundColor
 
-    backgroundColor = invariantViewProperties.backgroundColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(label)
 
-    label.translatesAutoresizingMaskIntoConstraints = false
-    addSubview(label)
+        let edgeInsets = invariantViewProperties.edgeInsets
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: topAnchor, constant: edgeInsets.top),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: edgeInsets.bottom),
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: edgeInsets.leading),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: edgeInsets.trailing),
+        ])
+    }
 
-    let edgeInsets = invariantViewProperties.edgeInsets
-    NSLayoutConstraint.activate([
-      label.topAnchor.constraint(equalTo: topAnchor, constant: edgeInsets.top),
-      label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: edgeInsets.bottom),
-      label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: edgeInsets.leading),
-      label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: edgeInsets.trailing),
-    ])
-  }
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-  @available(*, unavailable)
-  required init?(coder _: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
+    // MARK: Fileprivate
 
-  // MARK: Fileprivate
+    fileprivate func setContent(_ content: Content) {
+        label.text = content.monthText
+        accessibilityLabel = content.accessibilityLabel
+    }
 
-  fileprivate func setContent(_ content: Content) {
-    label.text = content.monthText
-    accessibilityLabel = content.accessibilityLabel
-  }
+    // MARK: Private
 
-  // MARK: Private
-
-  private let invariantViewProperties: InvariantViewProperties
-  private let label: UILabel
-
+    private let invariantViewProperties: InvariantViewProperties
+    private let label: UILabel
 }
 
 // MARK: Accessibility
 
-extension MonthHeaderView {
+public extension MonthHeaderView {
+    override var isAccessibilityElement: Bool {
+        get { true }
+        set {}
+    }
 
-  public override var isAccessibilityElement: Bool {
-    get { true }
-    set { }
-  }
-
-  public override var accessibilityTraits: UIAccessibilityTraits {
-    get { invariantViewProperties.accessibilityTraits }
-    set { }
-  }
-
+    override var accessibilityTraits: UIAccessibilityTraits {
+        get { invariantViewProperties.accessibilityTraits }
+        set {}
+    }
 }
 
 // MARK: MonthHeaderView.Content
 
-extension MonthHeaderView {
+public extension MonthHeaderView {
+    /// Encapsulates the data used to populate a `MonthHeaderView`'s text label. Use a `DateFormatter` to create the
+    /// `monthText` and `accessibilityLabel` strings.
+    ///
+    /// - Note: To avoid performance issues, reuse the same `DateFormatter` for each month, rather than creating
+    /// a new `DateFormatter` for each month.
+    struct Content: Equatable {
+        // MARK: Lifecycle
 
-  /// Encapsulates the data used to populate a `MonthHeaderView`'s text label. Use a `DateFormatter` to create the
-  /// `monthText` and `accessibilityLabel` strings.
-  ///
-  /// - Note: To avoid performance issues, reuse the same `DateFormatter` for each month, rather than creating
-  /// a new `DateFormatter` for each month.
-  public struct Content: Equatable {
+        public init(monthText: String, accessibilityLabel: String?) {
+            self.monthText = monthText
+            self.accessibilityLabel = accessibilityLabel
+        }
 
-    // MARK: Lifecycle
+        // MARK: Public
 
-    public init(monthText: String, accessibilityLabel: String?) {
-      self.monthText = monthText
-      self.accessibilityLabel = accessibilityLabel
+        public let monthText: String
+        public let accessibilityLabel: String?
     }
-
-    // MARK: Public
-
-    public let monthText: String
-    public let accessibilityLabel: String?
-  }
-
 }
 
 // MARK: MonthHeaderView.InvariantViewProperties
 
-extension MonthHeaderView {
+public extension MonthHeaderView {
+    /// Encapsulates configurable properties that change the appearance and behavior of `MonthHeaderView`. These cannot be
+    /// changed after a `MonthHeaderView` is initialized.
+    struct InvariantViewProperties: Hashable {
+        // MARK: Lifecycle
 
-  /// Encapsulates configurable properties that change the appearance and behavior of `MonthHeaderView`. These cannot be
-  /// changed after a `MonthHeaderView` is initialized.
-  public struct InvariantViewProperties: Hashable {
+        private init() {}
 
-    // MARK: Lifecycle
+        // MARK: Public
 
-    private init() { }
+        public static let base = InvariantViewProperties()
 
-    // MARK: Public
+        /// The background color of the entire view, unaffected by `edgeInsets`.
+        public var backgroundColor = UIColor.clear
 
-    public static let base = InvariantViewProperties()
+        /// Edge insets that change the position of the month's label.
+        public var edgeInsets = NSDirectionalEdgeInsets.zero
 
-    /// The background color of the entire view, unaffected by `edgeInsets`.
-    public var backgroundColor = UIColor.clear
+        /// The font of the month's label.
+        public var font = UIFont.systemFont(ofSize: 22)
 
-    /// Edge insets that change the position of the month's label.
-    public var edgeInsets = NSDirectionalEdgeInsets.zero
+        /// The text alignment of the month's label.
+        public var textAlignment = NSTextAlignment.natural
 
-    /// The font of the month's label.
-    public var font = UIFont.systemFont(ofSize: 22)
+        /// The text color of the month's label.
+        public var textColor: UIColor = if #available(iOS 13.0, *) {
+            .label
+        } else {
+            .black
+        }
 
-    /// The text alignment of the month's label.
-    public var textAlignment = NSTextAlignment.natural
+        /// The accessibility traits of the `MonthHeaderView`.
+        public var accessibilityTraits = UIAccessibilityTraits.header
 
-    /// The text color of the month's label.
-    public var textColor: UIColor = {
-      if #available(iOS 13.0, *) {
-        return .label
-      } else {
-        return .black
-      }
-    }()
-
-    /// The accessibility traits of the `MonthHeaderView`.
-    public var accessibilityTraits = UIAccessibilityTraits.header
-
-    public func hash(into hasher: inout Hasher) {
-      hasher.combine(backgroundColor)
-      hasher.combine(edgeInsets.leading)
-      hasher.combine(edgeInsets.trailing)
-      hasher.combine(edgeInsets.top)
-      hasher.combine(edgeInsets.bottom)
-      hasher.combine(font)
-      hasher.combine(textAlignment)
-      hasher.combine(textColor)
-      hasher.combine(accessibilityTraits)
+        public func hash(into hasher: inout Hasher) {
+            hasher.combine(backgroundColor)
+            hasher.combine(edgeInsets.leading)
+            hasher.combine(edgeInsets.trailing)
+            hasher.combine(edgeInsets.top)
+            hasher.combine(edgeInsets.bottom)
+            hasher.combine(font)
+            hasher.combine(textAlignment)
+            hasher.combine(textColor)
+            hasher.combine(accessibilityTraits)
+        }
     }
-
-  }
-
 }
 
 // MARK: CalendarItemViewRepresentable
 
 extension MonthHeaderView: CalendarItemViewRepresentable {
+    public static func makeView(
+        withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
+        -> MonthHeaderView
+    {
+        MonthHeaderView(invariantViewProperties: invariantViewProperties)
+    }
 
-  public static func makeView(
-    withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
-    -> MonthHeaderView
-  {
-    MonthHeaderView(invariantViewProperties: invariantViewProperties)
-  }
-
-  public static func setContent(_ content: Content, on view: MonthHeaderView) {
-    view.setContent(content)
-  }
-
+    public static func setContent(_ content: Content, on view: MonthHeaderView) {
+        view.setContent(content)
+    }
 }
