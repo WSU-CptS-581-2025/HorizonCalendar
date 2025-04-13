f41e57acb3795c265e0a9ffe3e216a72581dac92
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-04-13 12:23:01 -0700

    Fix spacing (PartialMonthVisibilityDemoViewController)

--- Example/HorizonCalendarExample/HorizonCalendarExample/Demo View Controllers/PartialMonthVisibilityDemoViewController.swift
+++ Example/HorizonCalendarExample/HorizonCalendarExample/Demo View Controllers/PartialMonthVisibilityDemoViewController.swift
@@ -5,57 +5,57 @@ import HorizonCalendar
 import UIKit
 
 final class PartialMonthVisibilityDemoViewController: BaseDemoViewController {
+    // MARK: Internal
 
-  // MARK: Internal
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+        title = "Partial Month Visibility"
 
-    title = "Partial Month Visibility"
+        calendarView.daySelectionHandler = { [weak self] day in
+            guard let self else { return }
 
-    calendarView.daySelectionHandler = { [weak self] day in
-      guard let self else { return }
-
-      selectedDate = calendar.date(from: day.components)
-      calendarView.setContent(makeContent())
-    }
-  }
-
-  override func makeContent() -> CalendarViewContent {
-    let startDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 16))!
-    let endDate = calendar.date(from: DateComponents(year: 2020, month: 12, day: 05))!
-
-    let selectedDate = selectedDate
-
-    return CalendarViewContent(
-      calendar: calendar,
-      visibleDateRange: startDate...endDate,
-      monthsLayout: monthsLayout)
-
-      .interMonthSpacing(24)
-      .verticalDayMargin(8)
-      .horizontalDayMargin(8)
-
-      .dayItemProvider { [calendar, dayDateFormatter] day in
-        var invariantViewProperties = DayView.InvariantViewProperties.baseInteractive
-
-        let date = calendar.date(from: day.components)
-        if date == selectedDate {
-          invariantViewProperties.backgroundShapeDrawingConfig.borderColor = .blue
-          invariantViewProperties.backgroundShapeDrawingConfig.fillColor = .blue.withAlphaComponent(0.15)
+            selectedDate = calendar.date(from: day.components)
+            calendarView.setContent(makeContent())
         }
+    }
 
-        return DayView.calendarItemModel(
-          invariantViewProperties: invariantViewProperties,
-          content: .init(
-            dayText: "\(day.day)",
-            accessibilityLabel: date.map { dayDateFormatter.string(from: $0) },
-            accessibilityHint: nil))
-      }
-  }
+    override func makeContent() -> CalendarViewContent {
+        let startDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 16))!
+        let endDate = calendar.date(from: DateComponents(year: 2020, month: 12, day: 05))!
 
-  // MARK: Private
+        let selectedDate = selectedDate
 
-  private var selectedDate: Date?
+        return CalendarViewContent(
+            calendar: calendar,
+            visibleDateRange: startDate ... endDate,
+            monthsLayout: monthsLayout
+        )
 
+        .interMonthSpacing(24)
+        .verticalDayMargin(8)
+        .horizontalDayMargin(8)
+        .dayItemProvider { [calendar, dayDateFormatter] day in
+            var invariantViewProperties = DayView.InvariantViewProperties.baseInteractive
+
+            let date = calendar.date(from: day.components)
+            if date == selectedDate {
+                invariantViewProperties.backgroundShapeDrawingConfig.borderColor = .blue
+                invariantViewProperties.backgroundShapeDrawingConfig.fillColor = .blue.withAlphaComponent(0.15)
+            }
+
+            return DayView.calendarItemModel(
+                invariantViewProperties: invariantViewProperties,
+                content: .init(
+                    dayText: "\(day.day)",
+                    accessibilityLabel: date.map { dayDateFormatter.string(from: $0) },
+                    accessibilityHint: nil
+                )
+            )
+        }
+    }
+
+    // MARK: Private
+
+    private var selectedDate: Date?
 }
