07c43c207cbabea9dd6a5a887a2b369c74795027
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2025-04-13 15:39:07 -0700

    Fix spacing (DayOfWeekPosition)

--- Sources/Public/DayOfWeekPosition.swift
+++ Sources/Public/DayOfWeekPosition.swift
@@ -20,29 +20,25 @@ import Foundation
 /// Represents a day of the week. In the Gregorian calendar, the first position is Sunday and the last position is Saturday. All calendars
 /// in `Foundation.Calendar` have 7 day of the week positions.
 public enum DayOfWeekPosition: Int, CaseIterable, Hashable {
+    // MARK: Public
 
-  // MARK: Public
+    case first = 1
+    case second
+    case third
+    case fourth
+    case fifth
+    case sixth
+    case last
 
-  case first = 1
-  case second
-  case third
-  case fourth
-  case fifth
-  case sixth
-  case last
+    // MARK: Internal
 
-  // MARK: Internal
-
-  static let numberOfPositions = 7
-
+    static let numberOfPositions = 7
 }
 
 // MARK: Comparable
 
 extension DayOfWeekPosition: Comparable {
-
-  public static func < (lhs: DayOfWeekPosition, rhs: DayOfWeekPosition) -> Bool {
-    lhs.rawValue < rhs.rawValue
-  }
-
+    public static func < (lhs: DayOfWeekPosition, rhs: DayOfWeekPosition) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
 }
